de72daf99f6bdfa3ac8ce2e6b81973c516349486
Sander Brokelman <sander@vuurbaard.com>
2023-09-29 12:34:23 +0000

    Dynamic list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+
+<h1>A11y</h1>
+<p>This is <a href="//vuurbaard.com">Vuurbaard</a>'s accessibility test project.</p>
+
+<nav>
+    <ul aria-label="Main navigation">
+        <li>
+            <a href="/lists">Lists</a>
+        </li>
+    </ul>
+</nav>
---
+++ src/routes/lists/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	let listItems = ['Item 1', 'Item 2', 'Item 3'];
+</script>
+
+<nav aria-label="Main navigation">
+    <a href="/">⬅️ Go back</a>
+</nav>
+
+<h1>Lists</h1>
+
+<main>
+    <section aria-label="Dynamic list">
+        <h2>Dynamic list</h2>
+        <button on:click={() => listItems = [...listItems, `Item ${listItems.length + 1}`]}>Add item</button>
+        <button on:click={() => {listItems.pop(); listItems = listItems} }>Remove item</button>
+        <ul aria-live="polite">
+            {#each listItems as item}
+            <li>{item}</li>
+            {/each}
+        </ul>
+    </section>
+</main>
